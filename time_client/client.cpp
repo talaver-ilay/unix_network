30cb990f0c2130db40e3671a717884715762b315
linux_vb <varenandrej@gmail.com>
2024-07-24 16:00:03 +0300

    error

--- time_client/client.cpp
+++ time_client/client.cpp
@@ -1,5 +1,17 @@
-#include <iostream>
-#include "../Unix-Network-Programming-master/lib/unp.h"
+#include    <iostream>
+#include	<sys/types.h>	/* basic system data types */
+#include	<sys/socket.h>	/* basic socket definitions */
+#include	<netinet/in.h>
+#include    "error.h"
+#include	<unistd.h>
+#include	<string.h>
+
+#define	bzero(ptr,n)		memset(ptr, 0, n)
+
+#define	SA	struct sockaddr
+
+int inet_pton(int family, const char *strptr, void *addrptr);
+int inet_aton(const char *cp, struct in_addr *ap);
 
 int main(int argc, char **argv){
     int sockfd, n;
@@ -22,4 +34,74 @@ int main(int argc, char **argv){
 
 
     exit(0);
+}
+
+int
+inet_pton(int family, const char *strptr, void *addrptr)
+{
+    if (family == AF_INET) {
+    	struct in_addr  in_val;
+
+        if (inet_aton(strptr, &in_val)) {
+            memcpy(addrptr, &in_val, sizeof(struct in_addr));
+            return (1);
+        }
+		return(0);
+    }
+	errno = EAFNOSUPPORT;
+    return (-1);
+}
+int
+inet_aton(const char *cp, struct in_addr *ap)
+{
+    int dots = 0;
+    register u_long acc = 0, addr = 0;
+
+    do {
+	register char cc = *cp;
+
+	switch (cc) {
+	case '0':
+	case '1':
+	case '2':
+	case '3':
+	case '4':
+	case '5':
+	case '6':
+	case '7':
+	case '8':
+	case '9':
+	    acc = acc * 10 + (cc - '0');
+	    break;
+
+	case '.':
+	    if (++dots > 3) {
+		return 0;
+	    }
+	    /* Fall through */
+
+	case '\0':
+	    if (acc > 255) {
+		return 0;
+	    }
+	    addr = addr << 8 | acc;
+	    acc = 0;
+	    break;
+
+	default:
+	    return 0;
+	}
+    } while (*cp++) ;
+
+    /* Normalize the address */
+    if (dots < 3) {
+	addr <<= 8 * (3 - dots) ;
+    }
+
+    /* Store it if requested */
+    if (ap) {
+	ap->s_addr = htonl(addr);
+    }
+
+    return 1;    
 }
---
+++ time_client/error.h
@@ -0,0 +1,7 @@
+#pragma once
+#define	MAXLINE		4096
+void	 err_dump(const char *, ...);
+void	 err_msg(const char *, ...);
+void	 err_quit(const char *, ...);
+void	 err_ret(const char *, ...);
+void	 err_sys(const char *, ...);
